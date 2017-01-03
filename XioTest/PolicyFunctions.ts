84fddee0abb12ce24004c3a1d5f5274fdc411f7f
ra81 <ra8181@mail.ru>
2017-01-04 01:14:42 +0700

    все функции выполнения политик заготовки положил в PolicyFunctions файлик

---
+++ XioTest/PolicyFunctions.ts
@@ -0,0 +1,97 @@
+﻿//
+// сюда кладем все функции которые собсна выполняют политики
+//
+function salePrice(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function salePolicy(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function servicePrice(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function serviceWithoutStockPrice(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function incineratorPrice(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function retailPrice(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function prodSupply(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function storeSupply(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function wareSupply(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function advertisement(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function salary(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function holiday(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function training(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function equipment(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function technology(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function research(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function prodBooster(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function politicAgitation(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
+
+function wareSize(policyName: string, subid: number, choices: number) {
+    let fn = getFuncName(arguments);
+    logDebug("started: ", fn);
+}
